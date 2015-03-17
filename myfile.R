d02cb943e5c188670fe784cb037f3a13c8d4785e
George Shirreff <georgeshirreff@gmail.com>
2015-03-17 14:56:15 -0400

    Second change

--- myfile.R
+++ myfile.R
@@ -1,1 +1,10 @@
-git add - add a new file or add a newly changed folder to the repo
+#git add - add a new file or add a newly changed folder to the repo
+#while you're making stuff and changing stuff, you can just keep saying add to make them into 
+#it's easy to undo a commit
+#it's harder to undo half a commit, so bundle up in a commit things that you would want to undo together i.e. interdependent changes. 
+
+#git commit automatically opens vi
+#git config --global user.name
+#git config --global user.email "sldkdslfj@dfdjskfjd.com"
+
+#git status is a good default command if you're not sure what to do next. 
